405a7457cdad1ae43629889c6027435f177a6829
Seema-Baghel <seemabaghel1997@gmail.com>
2020-03-07 18:19:57 +0530

    [Seema Baghel]Refactor. Checking the Employee is Present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,6 +2,16 @@
 
 echo "********** Welcome to Employee Wage Computation ***********"
 
+isPresent=0
+randomCheck=$((RANDOM%2));
+if [ $randomCheck -eq 1 ];
+then
+        isPresent="Present";
+else
+        isPresent="Absent";
+fi
+echo $isPresent;
+
 
 
 
