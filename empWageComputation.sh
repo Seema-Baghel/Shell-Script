9e7ed9cb69c48f5e476fe8a1d7fb7a0263698b76
Seema-Baghel <seemabaghel1997@gmail.com>
2020-03-08 22:04:59 +0530

    [Seema Baghel]Refactor. Checking Part Time and Full Time wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,12 +4,16 @@ echo "********** Welcome to Employee Wage Computation ***********"
 
 Emp_Wage_PerHr=20
 isFullTime=1
-randomCheck=$((RANDOM%2));
+isPartTime=2
+randomCheck=$((RANDOM%3));
 if [ $isFullTime -eq $randomCheck ];
 then
         empHr=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+        empHr=4;
 else
-        empHr=0;
+	empHr=0;
 fi
 salary=$(( $Emp_Wage_PerHr * $empHr ));
 echo $salary;
