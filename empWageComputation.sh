4a63572bbc30b40fd441d0a799f9d41055974614
Seema-Baghel <60878853+Seema-Baghel@users.noreply.github.com>
2020-03-08 22:21:20 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,23 +2,6 @@
 
 echo "********** Welcome to Employee Wage Computation ***********"
 
-Emp_Wage_PerHr=20
-isFullTime=1
-isPartTime=2
-randomCheck=$((RANDOM%3));
-if [ $isFullTime -eq $randomCheck ];
-then
-        empHr=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-        empHr=4;
-else
-	empHr=0;
-fi
-salary=$(( $Emp_Wage_PerHr * $empHr ));
-echo $salary;
-
-
 
 
 
