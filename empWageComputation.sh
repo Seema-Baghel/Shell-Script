0e953c0941cc73e21b3417eb7697ec0d0b805d85
Seema-Baghel <seemabaghel1997@gmail.com>
2020-03-09 12:42:59 +0530

    [Seema Baghel]Refactor. Calculating monthly wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,32 +2,29 @@
 
 echo "********** Welcome to Employee Wage Computation ***********"
 
-emp_Wage_PerHr=20
-isFullTime=2
-isPartTime=1
-total_Salary=0
-noOf_Working_Day=20
-noOf_Working_Hr=0
-Total_Working_Hr=100
-Working_Days=1
-while [ $noOf_Working_Hr -le $Total_Working_Hr ] && [ $Working_Days -le $noOf_Working_Day ]
+isFullTime=2;
+isHalfTime=1;
+totalSalary=0;
+empRatePerHr=20;
+noOfWorkingDay=20
+for (( day=1; day<=$noOfWorkingDay; day++ ))
 do
-        empCheck=$((RANDOM%3))
-	case $empCheck in
-        	$isFullTime)
-        	empHr=8;
-	;;
-        	$isHalfTime)
-        	empHr=4;
-	;;
-        	*)
-        	empHr=0;
-	esac
-	number=$(($noOf_Working_Hr+$empHr));
-	((Working_Days++))
-	salary=$(( $emp_Wage_PerHr * $empHr ));
-	total_Salary=$(($total_Salary+$salary));
+        randomCheck=$((RANDOM%3))
+case $randomCheck in
+        $isFullTime)
+        empHr=8;
+;;
+        $isHalfTime)
+        empHr=4;
+;;
+        *)
+        empHr=0;
+esac
+salary=$(( $empRatePerHr * $empHr ));
+totalSalary=$(($totalSalary+$salary));
 done
+
+
 
 
 
