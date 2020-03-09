19bc1be6c965d2aa434adcef721daf0833c29b9e
Seema-Baghel <seemabaghel1997@gmail.com>
2020-03-09 12:14:59 +0530

    [Seema Baghel]Refactor. Calculating total wages with condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,22 +2,32 @@
 
 echo "********** Welcome to Employee Wage Computation ***********"
 
-Emp_Wage_PerHr=20
+emp_Wage_PerHr=20
 isFullTime=2
 isPartTime=1
-empCheck=$((RANDOM%3));
-case $empCheck in
-        $isHalfTime)
-        empHr=4;
-;;
-        $isFullTime)
-        empHr=8;
-;;
-        *)
-        empHr=0;
-esac
-salary=$(( $Emp_Wage_PerHr * $empHr ));
-echo $salary;
+total_Salary=0
+noOf_Working_Day=20
+noOf_Working_Hr=0
+Total_Working_Hr=100
+Working_Days=1
+while [ $noOf_Working_Hr -le $Total_Working_Hr ] && [ $Working_Days -le $noOf_Working_Day ]
+do
+        empCheck=$((RANDOM%3))
+	case $empCheck in
+        	$isFullTime)
+        	empHr=8;
+	;;
+        	$isHalfTime)
+        	empHr=4;
+	;;
+        	*)
+        	empHr=0;
+	esac
+	number=$(($noOf_Working_Hr+$empHr));
+	((Working_Days++))
+	salary=$(( $emp_Wage_PerHr * $empHr ));
+	total_Salary=$(($total_Salary+$salary));
+done
 
 
 
