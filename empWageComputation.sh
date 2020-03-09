2019b41c9160e7b4d21a3bd9103ff3811b3699ff
Seema-Baghel <seemabaghel1997@gmail.com>
2020-03-09 14:26:26 +0530

    [Seema Baghel]Refactor. calculating wages using Array UC-8

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,39 +8,39 @@ total_salary=0;
 emp_Rate_PerHr=20;
 num_Working_Days=20;
 num_Working_Hrs=0;
-Total_Working_Hrs=100;
+Total_Working_Hrs=50;
 Working_days=1;
 function getWorkHr(){
- #empCheck=$((RANDOM%3));
 
-	case  $empCheck in
+	case $empCheck in
 	$isFullTime)
 	empHr=8;
- ;;
+;;
 	$isPartTime)
 	empHr=4;
 ;;
 	*)
-        empHr=0;
+	empHr=0;
 ;;
-	esac
+        esac
 	echo $empHr
 }
 function getEmpwage(){
 	
 	local empHr=$1
-	return $(($empHr*$emp_Rate_PerHr))
+	echo $(($empHr*$emp_Rate_PerHr))
 
 }
-while [ $num_Working_Hrs -le $Total_Working_Hrs  ] && [ $Working_days -le $num_Working_Days  ]
+while [ $num_Working_Hrs -le $Total_Working_Hrs  ] && [ $Working_days -le $num_Working_Days ]
 do
-        empCheck=$((RANDOM%3));
-	empHr="$(getWorkHr $empCheck)"
-        numWorkingHrs=$(($num_Working_Hrs+$empHr));
+	empCheck=$((RANDOM%3));
+        empHr="$(getWorkHr $empCheck)"
+        numWorkingHrs=$(($numWorkingHrs+$empHr));
         ((Working_days++))
-        salary=$(($emp_Rate_PerHr*$empHr))
-	getEmpwage $empHr
-        #total_salary=$?
-        total_salary=$(($total_salary+$salary));
+        salary=$(($emp_Rate_PerHr*$empHr));
+        dailyWage[$Working_days]="$( getEmpwage $empHr )"
 done
- echo "total salary:$total_salary"
+total_salary=$(($total_salary+$salary));
+echo ${dailyWage[@]}
+
+
